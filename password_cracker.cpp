def922377c952e79ca2c6a2581106d89f27426b1
David Chidester <31967957+dchid@users.noreply.github.com>
2019-05-01 19:17:06 -0700

    Added compare hash function

--- password_cracker.cpp
+++ password_cracker.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "md5.h"
+
 using namespace std;
+
+void bruteForce();
+void wordList();
+bool hashCompare(string plaintext, string hash);
 
 int main(){
     cout << "Welcome to the C++ password cracker\n";
@@ -9,7 +15,7 @@ int main(){
     string inputHash;
     cin >> inputHash;
     cout << "you entered " + inputHash + "\n";
-	cout << "Would you like to use a wordlist?\n (y/n)";
+	cout << "Would you like to use a wordlist?\n (y/n)\n";
     
     char yesOrNo;
     
@@ -23,6 +29,7 @@ int main(){
         else if (yesOrNo == 'n' || yesOrNo == 'N'){
             cout << "Ok brute forcing\n";
             inputInvalid = false;
+            bruteForce();
         }
         else cout << "Bad input: please type either 'y' or 'n'\n";
     }
@@ -39,43 +46,36 @@ int main(){
 void wordList(){
     cout << "please enter your wordlist\n";
     string plainText;
-    std::ifstream file("rockyou.txt");
-    std::string s;
-
-    while (std::getline(file, s)){
-
-   	 if (!file){ //testing if file opens
-         cout << "unable to open file";
-         exit(1);
-
-   	 if (file has next line){
-         s = file current line;
-	
-        }
-    else {
-        current line = next line;
-  }
+    bool matchfound = hashCompare("ponies", "47346fc7580de7596d7df8d115a3545d");
+    
+    //enter in wordList
+    
+    //While loop terminates when the list is done or a match has been found
+    //while (wordlist is not empty && plaintext.compare(hash) == 0){}
+    
+    //if a match has been found, output the matched plaintext
+    
+    //else cout << "sorry no match found";
 }
-}
-    string line;
-    string str;
-    string hash = "FF0EB2864FEB22354747F8C85D42CCB5"; //testing hash
-    bool found = false;
-
-	while (wordlist has next line && found == false){
-        str = wordlist current line;
-        string hashedStr = hash(str);
-        	if (hashedStr.compare(hash) == true){
-                print("the plain text string is " + str);
-                found = true
-        	}
-        	else {
-                current line = next line;
-        	}
-
-	}
 
 //brute force
 void bruteForce(){
-    
+    cout << "how long would you like your maximum string to be?\n";
+    int maxLength;
+    cin >> maxLength;
+    //making a command to give to system
+    string strcmd = "crunch 1 " + to_string(maxLength) + " -o output.txt";
+    cout << "\ngenerating strings\n";
+    const char *command = strcmd.c_str();
+    system(command);
+    wordList();
+}
+
+bool hashCompare(string plaintext, string hash){
+
+    string hashedString = md5(plaintext);
+
+    cout << hashedString + "\n" + hash;
+
+    return plaintext.compare(hash) == 0;
 }
